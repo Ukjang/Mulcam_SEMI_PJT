a9d66792805739d96e4e6f20e39572ab23c387ed
wdc35333 <wdc35333@gmail.com>
2022-05-17 17:57:20 +0900

    일부 문구 수정

--- Flask/temp_graph.py
+++ Flask/temp_graph.py
@@ -1,4 +1,4 @@
-
+from cv2 import rotate
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
@@ -39,7 +39,6 @@ def temp_graph(region, predict_month, temp):
     plt.plot(x, temperatures, label=f'{region}의 {temp_name}')
     plt.ylim(temperatures.min()-0.2, temperatures.max()+0.2)
     plt.xticks(x, years, rotation = 90)
-    
     plt.xlabel('년도')
     plt.ylabel('기온')
     plt.legend()
